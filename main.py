a7bfcdfc1bdc2c4e4c3aa33c530bebc4ca4f9af3
Ori <disori777@gmail.com>
2024-12-08 22:22:38 +0800

    Add class for creation of database model

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ app.config["SECRET_KEY"] = "xDxDxD"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email_add = db.Column(db.String(150))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
