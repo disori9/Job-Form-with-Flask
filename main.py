f17344cd82abd29301801829d17e1f260ab1af98
Ori <disori777@gmail.com>
2024-12-08 22:24:41 +0800

    Create a database file

--- main.py
+++ main.py
@@ -28,4 +28,8 @@ def index():
 
     return render_template("index.html")
 
-app.run(debug=True, port=5001)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
