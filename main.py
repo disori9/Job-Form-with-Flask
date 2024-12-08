146ae69c1dad58a76ce5e62dab3c6276152cff0f
Ori <disori777@gmail.com>
2024-12-08 22:10:43 +0800

    Implement condition for returning the link when the method is GET for request and getting the data from the user when submitting or "POST" for request

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email_add = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
 
 app.run(debug=True, port=5001)
