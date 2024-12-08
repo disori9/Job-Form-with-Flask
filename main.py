b94badbfff6bf06ced4561b0b8ddfc253cb83679
Ori <disori777@gmail.com>
2024-12-08 22:19:45 +0800

    Create variables for creation of database

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "xDxDxD"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
